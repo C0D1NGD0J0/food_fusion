cb6d3372a06395278f76c168cbf352635d666c0d
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2020-04-18 12:31:13 -0400

    about-us section added to landing page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,12 @@
 $(window).on("load", function () {
-  const t2 = new TimelineMax({ paused: false })
-  let splitText = new SplitText(".headline");
-  let splitText2 = new SplitText(".sub-headline");
-  t2.from('body', 2, {opacity: 0, ease: Expo.easeInOut})
-  t2.staggerFrom(splitText.chars, .3, { opacity: 0, y: -100, ease: Power1.easeIn }, .2)
-  t2.staggerFrom(splitText2.words, .5, { opacity: 0, y: 100, ease: Power2.easeIn }, .2)
-  t2.from(".hero-content__cta", 1, {opacity: 0});
-});
+  //const t2 = new TimelineMax({ paused: false })
+  //let splitText = new SplitText(".headline");
+  //let splitText2 = new SplitText(".sub-headline");
+  //t2.from('body', 2, {opacity: 0, ease: Expo.easeInOut})
+  //t2.staggerFrom(splitText.chars, .3, { opacity: 0, y: -100, ease: Power1.easeIn }, .2)
+  //t2.staggerFrom(splitText2.words, .5, { opacity: 0, y: 100, ease: Power2.easeIn }, .2)
+  //t2.from(".hero-content__cta", 1, {opacity: 0});
 
-$(function(){
   $(".link-item1").hover(function(){
     $(".bg1").fadeIn(600);
   }, function(){
